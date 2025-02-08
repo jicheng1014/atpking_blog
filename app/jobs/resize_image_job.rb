e56993cf36f0af03219dd0fc7f784c3c51a5769c
atpking <atpking@gmail.com>
2025-02-08 16:28:32 +0800

    fix job

--- app/jobs/resize_image_job.rb
+++ app/jobs/resize_image_job.rb
@@ -5,20 +5,28 @@ class ResizeImageJob < ApplicationJob
     return unless blob.image?
     return if blob.metadata['processed']
 
-    blob.open do |tempfile|
+    blob.open do |original|
       # 使用 image_processing gem 处理图片
       processed = ImageProcessing::MiniMagick
-        .source(tempfile)
+        .source(original)
         .resize_to_limit(1024, 1024)
         .call
 
-      # 更新 blob 的内容
-      blob.update!(
+      # 使用新的处理后的文件创建一个新的 blob
+      new_blob = ActiveStorage::Blob.create_and_upload!(
         io: File.open(processed.path),
         filename: blob.filename,
         content_type: blob.content_type,
         metadata: blob.metadata.merge('processed' => true)
       )
+
+      # 更新所有引用原始 blob 的附件，指向新的 blob
+      blob.attachments.each do |attachment|
+        attachment.update!(blob: new_blob)
+      end
+
+      # 删除原始 blob
+      blob.purge
     end
   end
 end
